a7cdc11c366547db2b8ca8adfb2d07cb54d9163c
Fatmeh Shuman <fatoumeh@macusers-MacBook-Pro.local>
2019-02-11 14:47:21 +0000

    comments seeded

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -2,7 +2,7 @@ const {
   articleData, topicData, userData, commentData,
 } = require('../data/index');
 
-const { convertFromEpoch, formatArticles } = require('../utils/index');
+const { formatArticles, formatComments, getArticleIds } = require('../utils/index');
 
 exports.seed = function (knex, Promise) {
   return knex.migrate
@@ -10,12 +10,19 @@ exports.seed = function (knex, Promise) {
     .then(() => knex.migrate.latest())
     .then(() => Promise.all([knex('users').insert(userData).returning('*'), knex('topics').insert(topicData).returning('*')]))
     .then(([userRows, topicRows]) => {
-      console.log(`User rows length: ${userRows.length} , topic rows length: ${topicRows.length}`);
       const formattedArticles = formatArticles(articleData);
       const articlesRows = knex('articles')
         .insert(formattedArticles)
         .returning('*');
       return Promise.all([userRows, topicRows, articlesRows]);
+    })
+    .then(([userRows, topicRows, articlesRows]) => {
+      const articleIdList = getArticleIds(articlesRows);
+      const formattedComments = formatComments(commentData, articleIdList);
+      const commentsRows = knex('comments')
+        .insert(formattedComments)
+        .returning('*');
+      return Promise.all([userRows, topicRows, articlesRows, commentsRows]);
     })
     .catch(error => console.log(error));
 };
--- db/utils/index.js
+++ db/utils/index.js
@@ -1,6 +1,19 @@
 
 const convertFromEpoch = epochTime => new Date(epochTime);
 
+const getArticleIds = articlesRows => articlesRows.map((articleRow) => {
+  const articleObj = {
+    title: articleRow.title,
+    article_id: articleRow.article_id,
+  };
+  return articleObj;
+});
+
+const getArticleId = (title, articleIdList) => {
+  const article = articleIdList.filter(articlesRow => articlesRow.title === title);
+  return article[0].article_id;
+};
+
 const formatArticles = (articles) => {
   articles.forEach((article) => {
     const epochTime = article.created_at;
@@ -9,18 +22,20 @@ const formatArticles = (articles) => {
   return articles;
 };
 
-module.exports = { convertFromEpoch, formatArticles };
+const formatComments = (comments, articlesRows) => {
+  comments.forEach((comment) => {
+    const epochTime = comment.created_at;
+    comment.created_at = convertFromEpoch(epochTime);
 
-/*
-Articles:
-change created at
- {
-    title: 'Running a Node App',
-    topic: 'coding',
-    author: 'jessjelly',
-    body:
-      'This is part two of a series on how to get up and running with Systemd and Node.js. This part dives deeper into how to successfully run your app with systemd long-term, and how to set it up in a production environment.',
-    created_at: 1471522072389,
-  }
+    const author = comment.created_by;
+    delete comment.created_by;
+    comment.author = author;
 
-*/
+    const article_id = getArticleId(comment.belongs_to, articlesRows);
+    delete comment.belongs_to;
+    comment.article_id = article_id;
+  });
+  return comments;
+};
+
+module.exports = { formatArticles, formatComments, getArticleIds };
