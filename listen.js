54d097ceb69cea7b2421105b4f08c4c0c9e3be81
Fatmeh Shuman <fatoumeh@macusers-MacBook-Pro.local>
2019-02-14 09:34:40 +0000

    changes for heroku

--- listen.js
+++ listen.js
@@ -1,8 +1,9 @@
 const app = require('./app');
 
-const PORT = 9090;
+const PORT = process.env.PORT || 9090;
 
 app.listen(PORT, (err) => {
+  console.log(process.env.NODE_ENV);
   if (err) console.log('ERROR', err);
   else console.log(`server listening on port ${PORT}`);
 });
