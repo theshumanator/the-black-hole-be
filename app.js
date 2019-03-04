e9e9743434adeab66f28cb8503c01f81296e2e9d
Fatmeh Shuman <fatoumeh@macusers-MacBook-Pro.local>
2019-03-04 15:00:08 +0000

    added cors header

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const app = require('express')();
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
+const cors = require('cors');
 const apiRouter = require('./routes/apiRouter');
 
 const { handleErrors } = require('./utils/errors');
 
+app.use(cors());
+app.options('*', cors());
+
+
 app.use(bodyParser.json());
 app.use(expressValidator());
+
+app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*']);
+  // res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  // res.append('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 
 app.use('/api', apiRouter);
 
