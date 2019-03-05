6fac86ed352c40dff5303b05132ec7b71448f1d9
Fatmeh Shuman <fatoumeh@macusers-MacBook-Pro.local>
2019-03-05 21:54:59 +0000

    fixed issue where date default to table creation rather than now date

--- db/migrations/20190211114617_create_articles_table.js
+++ db/migrations/20190211114617_create_articles_table.js
@@ -1,6 +1,5 @@
 
 exports.up = function (knex, Promise) {
-  const nowDate = new Date(Date.now());
   return knex.schema.createTable('articles', (articlesTable) => {
     articlesTable.increments('article_id').primary().notNullable();
     articlesTable.string('title').notNullable();
@@ -8,7 +7,7 @@ exports.up = function (knex, Promise) {
     articlesTable.integer('votes').notNullable().defaultTo(0);
     articlesTable.string('topic').notNullable().references('slug').inTable('topics');
     articlesTable.string('author').notNullable().references('username').inTable('users');
-    articlesTable.datetime('created_at').defaultTo(nowDate.toUTCString());
+    articlesTable.datetime('created_at').defaultTo(knex.fn.now());
   });
 };
 
--- db/migrations/20190211114620_create_comments_table.js
+++ db/migrations/20190211114620_create_comments_table.js
@@ -1,13 +1,12 @@
 
 exports.up = function (knex, Promise) {
-  const nowDate = new Date(Date.now());
   return knex.schema.createTable('comments', (commentsTable) => {
     commentsTable.increments('comment_id').primary().notNullable();
     commentsTable.string('author').notNullable().references('username').inTable('users'); // ref username in users
     commentsTable.integer('article_id').notNullable().references('article_id').inTable('articles')
       .onDelete('CASCADE'); // ref article_id in articles
     commentsTable.integer('votes').notNullable().defaultTo(0);
-    commentsTable.datetime('created_at').defaultTo(nowDate.toUTCString());
+    commentsTable.datetime('created_at').defaultTo(knex.fn.now());
     commentsTable.text('body').notNullable();
   });
 };
